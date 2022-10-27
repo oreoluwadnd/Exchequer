598391102d910096b0d9e54fd76fb36ae39f20ea
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-10-27 17:14:24 +0100

    :sparkles: deployment prep

--- app.js
+++ app.js
@@ -1,18 +1,87 @@
 const express = require('express');
 const morgan = require('morgan');
+const ratelimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+
 const error = require('./middlewares/error');
 
 const userRoute = require('./routes/userRoutes');
 const accountRoute = require('./routes/accountRoutes');
 
+// Start express app
 const app = express();
+//trust proxy
+app.enable('trust proxy');
+//implement cors
+app.use(cors());
+//set security http headers
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:'],
+
+      fontSrc: ["'self'", 'https:', 'data:'],
+
+      scriptSrc: ["'self'", 'https://*.cloudflare.com'],
+
+      scriptSrcElem: ["'self'", 'https:', 'https://*.cloudflare.com'],
+
+      styleSrc: ["'self'", 'https:', 'unsafe-inline'],
+
+      connectSrc: ["'self'", 'data', 'https://*.cloudflare.com'],
+    },
+  })
+);
+//development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+//limit requests from same api
+const limiter = ratelimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+//body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
+//data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//data sanitization against XSS
+app.use(xss());
+//prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'amount',
+      'tag',
+      'phone',
+      'name',
+      'email',
+      'password',
+      'passwordConfirm',
+      'passwordCurrent',
+      'passwordNew',
+    ],
+  })
+);
+//compress text sent to client
+app.use(compression());
+
 app.use(express.json());
-app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//routes
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/account', accountRoute);
 
