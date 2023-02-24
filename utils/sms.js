70852bfc5db1db6f87dc40921aad6ca3bdd4fe86
Emmanuel Oreoluwa <awuloero13@gmail.com>
2023-02-24 22:50:53 +0100

    :sparkles: ci fix

--- utils/sms.js
+++ utils/sms.js
@@ -1,20 +1,20 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
-module.exports = class Sms {
-  constructor(user) {
-    this.phone = user.phone;
-    this.from = process.env.TWILIO_PHONE_NUMBER;
-  }
+// module.exports = class Sms {
+//   constructor(user) {
+//     this.phone = user.phone;
+//     this.from = process.env.TWILIO_PHONE_NUMBER;
+//   }
 
-  sendOtp(otp) {
-    client.messages
-      .create({
-        body: `Welcome to Exchequer! Your verification code is ${otp} and it expires in 5 minutes`,
-        from: this.from,
-        to: this.phone,
-      })
-      .then((message) => console.log(message.sid));
-  }
-};
+//   sendOtp(otp) {
+//     client.messages
+//       .create({
+//         body: `Welcome to Exchequer! Your verification code is ${otp} and it expires in 5 minutes`,
+//         from: this.from,
+//         to: this.phone,
+//       })
+//       .then((message) => console.log(message.sid));
+//   }
+// };
